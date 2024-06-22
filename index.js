19d2f46f71e9cc262d4f7b9ba1933f82527da8e3
sahan6524 <sahan@shvasa.com>
2024-06-22 14:32:18 +0530

    add cors error fix

--- index.js
+++ index.js
@@ -5,20 +5,9 @@ import codeReviewRoutes from "./routes/CodeReviewRoutes.js";
 import {rateLimiter} from "./middleware/rateLimiter.js";
 
 dotenv.config()
-const allowedOrigins = ['https://ai-code-review-pro.vercel.app', 'http://localhost:3000'];
-const corsOptions = {
-    origin: function (origin, callback) {
-        if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
-            callback(null, true)
-        } else {
-            callback(new Error('Not allowed by CORS'))
-        }
-    },
-    optionsSuccessStatus: 200
-};
 
 const app = express();
-app.use(cors(corsOptions))
+app.use(cors())
 app.use(rateLimiter)
 app.use(express.json({limit: "30mb", extended: true}))
 app.use('/api/v1/', codeReviewRoutes)
